ec564076c8075b787f4eff189719af56f365cff0
Diego Alejandro <info@zariguella.com>
2016-04-28 19:35:50 -0500

    Cree la primera clase de nuestro proyecto

---
+++ src/modelo/dto/Paciente.java
@@ -0,0 +1,41 @@
+package modelo.dto;
+
+public class Paciente {
+	int id;
+	String nombre;
+	String cedula;
+	String telefono;
+	String direccion;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getCedula() {
+		return cedula;
+	}
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	
+	
+}
