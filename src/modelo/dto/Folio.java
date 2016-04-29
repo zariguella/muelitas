cb697c09bf815ebd223498d428e8d6aa4021d7a3
Diego Alejandro <info@zariguella.com>
2016-04-28 20:10:17 -0500

    Creacion de la clase folio y la clase medico.

---
+++ src/modelo/dto/Folio.java
@@ -0,0 +1,47 @@
+package modelo.dto;
+
+import java.util.Date;
+
+public class Folio {
+	
+	int id;	
+	Paciente paciente;
+	Date fecha;
+	String motivoConsulta;
+	Medico medico;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Paciente getPaciente() {
+		return paciente;
+	}
+	public void setPaciente(Paciente paciente) {
+		this.paciente = paciente;
+	}
+	public Date getFecha() {
+		return fecha;
+	}
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	public String getMotivoConsulta() {
+		return motivoConsulta;
+	}
+	public void setMotivoConsulta(String motivoConsulta) {
+		this.motivoConsulta = motivoConsulta;
+	}
+	public Medico getMedico() {
+		return medico;
+	}
+	public void setMedico(Medico medico) {
+		this.medico = medico;
+	}
+	
+	
+	
+	
+
+}
---
+++ src/modelo/dto/Medico.java
@@ -0,0 +1,35 @@
+package modelo.dto;
+
+public class Medico {
+	int id;
+	String nombre;
+	String telefono;
+	String registroMedico;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	public String getRegistroMedico() {
+		return registroMedico;
+	}
+	public void setRegistroMedico(String registroMedico) {
+		this.registroMedico = registroMedico;
+	}
+	
+	
+
+}
