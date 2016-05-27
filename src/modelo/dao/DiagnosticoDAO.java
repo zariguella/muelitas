4a0cd26995a291e9a7c4af6181b046ab6c2eee2a
Diego Alejandro <info@zariguella.com>
2016-05-26 20:51:27 -0500

    avance mayo 26

--- src/modelo/dao/DiagnosticoDAO.java
+++ src/modelo/dao/DiagnosticoDAO.java
@@ -2,6 +2,46 @@ package modelo.dao;
 
 public class DiagnosticoDAO {
 	
-	public boolean
+	public boolean crearDiagnostico(DiagnosticoDTO diagnostico){
+		ConexionPostgres conexion=ConexionPostgres.obtenerInstancia();
+		conexion.conectar();
+		String consulta="insert into diagnostico(nombre,codigo)values('"+diagnostico.getNombre()+"','"+diagnostico.getCodigo()+"')";
+		ResultSet resultado=conexion.consultaSQL(consulta);
+		conexion.desconectar();	
+		if(resultado==null){
+			return false;
+		}else{
+			return true;
+		}		
+	}
+	
+	public boolean eliminarDiagnostico(DiagnosticoDTO diagnostico){
+		ConexionPostgres conexion=ConexionPostgres.obtenerInstancia();
+		conexion.conectar();
+		String consulta="delete from diagnostico where id='"+diagnostico.getId()+"'";
+		ResultSet resultado=conexion.consultaSQL(consulta);
+		conexion.desconectar();	
+		if(resultado==null){
+			return false;
+		}else{
+			return true;
+		}		
+	}
+	
+	public ArrayList<DiagnosticoDTO> listarDiagnostico(DiagnosticoDTO diagnostico){
+		try {
+			ConexionPostgres conexion=ConexionPostgres.obtenerInstancia();
+			conexion.conectar();
+			String consulta="select * from diagnostico";			
+			ResultSet resultado=conexion.consultaSQL(consulta);			
+				while(resultado.next()){
+					
+				}			
+			conexion.desconectar();		
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
 }
